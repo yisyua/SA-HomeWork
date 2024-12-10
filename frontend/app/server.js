001d64750d18014d0bc1e66aef5c12a1b23fd1fa
YITZZZZZZ <yitz0528@gmail.com>
2024-12-10 22:35:16 +0800

    Update server.js

--- frontend/app/server.js
+++ frontend/app/server.js
@@ -24,8 +24,8 @@ sql.connect(config).then(pool => {
 
     app.use('/api', controllers);
 
-    const server = app.listen(3000, () => {
-        console.log('Server is running on port 3000');
+    const server = app.listen(3251, () => {
+        console.log('Server is running on port 3251');
     });
 
     viteExpress.bind(app, server);
